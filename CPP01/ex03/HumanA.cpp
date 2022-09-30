1f91c533225daa69b6db3119fa7ca499b6fb15b0
jacinthe <jbouyer@student.42.fr>
2022-09-30 16:56:12 +0200

    a revoir

--- CPP01/ex03/HumanA.cpp
+++ CPP01/ex03/HumanA.cpp
@@ -6,7 +6,23 @@
 /*   By: jbouyer <jbouyer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/29 17:32:28 by jbouyer           #+#    #+#             */
-/*   Updated: 2022/09/29 17:32:42 by jbouyer          ###   ########.fr       */
+/*   Updated: 2022/09/30 15:19:53 by jbouyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "HumanA.hpp"
+
+HumanA :: HumanA(std::string name, Weapon weapon): _name(name), _weapon(weapon)
+{
+	return;
+}
+
+HumanA::~HumanA()
+{
+	return;
+}
+
+void	HumanA::Attack_message()
+{
+	std::cout<<this->_name<<"attacks with their"<<this _weapon.type<<std::endl;
+}
--- CPP01/ex03/HumanA.hpp
+++ CPP01/ex03/HumanA.hpp
@@ -6,14 +6,29 @@
 /*   By: jbouyer <jbouyer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/29 17:32:35 by jbouyer           #+#    #+#             */
-/*   Updated: 2022/09/29 17:41:15 by jbouyer          ###   ########.fr       */
+/*   Updated: 2022/09/30 15:27:22 by jbouyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef HUMANA_HPP
+# define HUMANA_HPP
+
+#include "Weapon.hpp"
+#include <string>
 
 class HumanA
 {
 	private :
 			std::string _name;
-			Weapon _weapon;
+			Weapon &_weapon;
+	
+	public :
+			HumanA(Weapon _weapon);
+			~HumanA();
+			void attack_message() const;
+			std::string getName() const;
+
 
 };
+
+#endif
