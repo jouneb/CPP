b48566db6c1453dd6c251e31259304aa2830853a
jacinthe <jbouyer@student.42.fr>
2022-10-17 18:55:45 +0200

    compris les fixed

--- CPP02/ex02/Fixed.hpp
+++ CPP02/ex02/Fixed.hpp
@@ -5,10 +5,11 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: jbouyer <jbouyer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/13 15:47:11 by jbouyer           #+#    #+#             */
-/*   Updated: 2022/10/17 15:47:29 by jbouyer          ###   ########.fr       */
+/*   Created: 2022/10/17 15:56:54 by jbouyer           #+#    #+#             */
+/*   Updated: 2022/10/17 16:03:03 by jbouyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 
 #ifndef FIXED_HPP
 # define FIXED_HPP
@@ -22,20 +23,29 @@ class Fixed
 			int					_rawBits;
 			static const int	_binaryPos;
 	public:
+	
+	//Constructors
 			Fixed();
 			Fixed(Fixed const &Fixed);
 			Fixed(const int i);
 			Fixed(const float f);
-			~Fixed();
-			Fixed& operator=(Fixed const &Fixed);
+			~Fixed(); //Destructor
+	
 
+
+	//Accesseurs les getteurs sont toujours const.
 			int		getRawBits(void) const;
-			void	setRawBits(int const raw);
+			void	setRawBits(int const raw);			
+			int		getBinaryPos(void) const;
+	
+	//Fonctions membres
 			float	toFloat(void) const;
 			int		toInt(void) const;
-			int		getBinaryPos(void) const;
+	
+	//Surcharge operateur
+			Fixed& operator=(Fixed const &Fixed);
 };
 
-std::ostream &operator<<(std::ostream &o, Fixed const& rhs);
+std::ostream &operator<<(std::ostream &o, Fixed const& rhs); //cas partiuclier de << independant de la classe.
 
 #endif
